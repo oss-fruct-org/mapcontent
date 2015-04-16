d8fe10f542f7edcced1b2ef964b48f83ac0b5c4c
Kirill Ivashov <ivashov@cs.petrsu.ru>
2015-04-16 15:10:25 +0300

    Update to api 22

---
+++ src/main/java/org/fruct/oss/mapcontent/content/helper/ContentHelper.java
@@ -0,0 +1,15 @@
+package org.fruct.oss.mapcontent.content.helper;
+
+public class ContentHelper {
+	public void enableLocationProviderNotifications() {
+
+	}
+
+	public void enableContentNotifications() {
+
+	}
+
+	public void enableNetworkNotifications() {
+
+	}
+}
